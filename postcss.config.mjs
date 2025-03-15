33ca779c2e9b92257ba827f4af0d9772a6a8de3b
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-03-15 02:22:10 -0600

    fixing

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+/* export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+}; */
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
